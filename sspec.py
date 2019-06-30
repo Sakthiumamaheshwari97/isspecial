2f3961ac4576fca8c9f989a6b1775c2afa0e1285
Sakthiumamaheshwari97 <51943276+Sakthiumamaheshwari97@users.noreply.github.com>
2019-06-30 03:48:14 -0700

    Update sspec.py

--- sspec.py
+++ sspec.py
@@ -1,7 +1,7 @@
 gh=input()
 dol=0
 for i in range(len(gh)):
-    if(gh[i].isdigit() or gh[i].alpha() or gh[i].(" ")):
+    if(gh[i].isdigit() or gh[i].alpha() or gh[i]==(" ")):
            continue
     else:
        dol+=1
